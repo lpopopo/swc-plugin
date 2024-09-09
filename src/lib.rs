7534ace21012d8d60b7df2b39b428997ec9125f5
liushuai166 <liushuai166@ke.com>
2024-09-09 15:30:05 +0800

    feat:async 函数添加try-catch

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-use core::fmt;
-use std::borrow::{Borrow, BorrowMut};
-
 use swc_common::SyntaxContext;
-use swc_common::{plugin::metadata::TransformPluginMetadataContextKind, Spanned};
-use swc_core::atoms::Atom;
 use swc_core::common::{Span, DUMMY_SP};
 use swc_core::ecma::ast;
 use swc_core::ecma::atoms::JsWord;
@@ -15,6 +10,11 @@ use swc_core::{
     },
     plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
 };
+
+mod async_tool;
+mod promise_tool;
+use async_tool::{already_wrapped, wrap_arrow_body_with_try_catch, wrap_with_try_catch};
+use promise_tool::{create_new_catch_callee, has_catch};
 
 pub struct TransformVisitor;
 
@@ -31,176 +31,51 @@ impl VisitMut for TransformVisitor {
                 if let MemberProp::Ident(IdentName { sym, .. }) = prop {
                     if sym == "then" && !has_catch(call_expr) {
                         // Create the console.error(err) statement
-                        let console_error_stmt = Stmt::Expr(ExprStmt {
-                            expr: Box::new(Expr::Call(CallExpr {
-                                span: DUMMY_SP,
-                                callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
-                                    span: DUMMY_SP,
-                                    obj: Box::new(Expr::Ident(Ident::new(
-                                        "console".into(),
-                                        DUMMY_SP,
-                                        SyntaxContext::empty(),
-                                    ))),
-                                    prop: MemberProp::Ident(IdentName::new(
-                                        "error".into(),
-                                        DUMMY_SP,
-                                    )),
-                                }))),
-                                args: vec![ExprOrSpread {
-                                    spread: None,
-                                    expr: Box::new(Expr::Ident(Ident::new(
-                                        "err".into(),
-                                        DUMMY_SP,
-                                        SyntaxContext::empty(),
-                                    ))),
-                                }],
-                                type_args: None,
-                                ctxt: SyntaxContext::empty(),
-                            })),
-                            span: DUMMY_SP,
-                        });
-
-                        // Create the arrow function
-                        let arrow_func = Expr::Arrow(ArrowExpr {
-                            span: DUMMY_SP,
-                            params: vec![Pat::Ident(BindingIdent::from(Ident::new(
-                                "err".into(),
-                                DUMMY_SP,
-                                SyntaxContext::empty(),
-                            )))],
-                            body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
-                                span: DUMMY_SP,
-                                stmts: vec![console_error_stmt],
-                                ctxt: SyntaxContext::empty(),
-                            })),
-                            is_async: false,
-                            is_generator: false,
-                            type_params: None,
-                            return_type: None,
-                            ctxt: SyntaxContext::empty(),
-                        });
-
-                        // Create the new function call with .catch
-                        let new_func = CallExpr {
-                            span: DUMMY_SP,
-                            callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
-                                span: DUMMY_SP,
-                                obj: Box::new(Expr::Call(call_expr.clone())),
-                                prop: MemberProp::Ident(IdentName::new("catch".into(), DUMMY_SP)),
-                            }))),
-                            args: vec![ExprOrSpread {
-                                spread: None,
-                                expr: Box::new(arrow_func),
-                            }],
-                            type_args: None,
-                            ctxt: SyntaxContext::empty(),
-                        };
-
-                        *call_expr = new_func;
+                        create_new_catch_callee(call_expr);
                     }
                 }
             }
         }
     }
-}
 
-fn get_next_call_expr(call_expr: &mut CallExpr) -> Option<&mut CallExpr> {
-    if let Callee::Expr(boxed_callee) = &mut call_expr.callee {
-        if let Expr::Member(MemberExpr { obj, prop, .. }) = &mut **boxed_callee {
-            if let MemberProp::Ident(IdentName { sym, .. }) = prop {
-                if sym == "then" {
-                    if let Expr::Call(next_call_expr) = &mut **obj {
-                        return Some(next_call_expr);
-                    }
+    fn visit_mut_fn_decl(&mut self, node: &mut FnDecl) {
+        // Check if options allow the transformation
+
+        // If the function is async and it's not already wrapped, wrap it with try-catch
+        if node.function.is_async {
+            if let Some(body) = &mut node.function.body {
+                if !already_wrapped(body) {
+                    wrap_with_try_catch(body);
                 }
             }
         }
-    }
-    None
-}
 
-fn has_catch(call_expr: &mut CallExpr) -> bool {
-    // println!("call_expr: {:?}\n\n", call_expr);
-    let mut current_expr = call_expr;
-    while let Some(next_expr) = get_next_call_expr(current_expr) {
-        if let Callee::Expr(boxed_callee) = &next_expr.callee {
-            if let Expr::Member(MemberExpr { prop, .. }) = &**boxed_callee {
-                if let MemberProp::Ident(IdentName { sym, .. }) = prop {
-                    println!("sym: {}\n\n", sym);
-                    if sym == "catch" {
-                        return true;
-                    }
+        // Continue visiting the node
+        node.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_fn_expr(&mut self, node: &mut FnExpr) {
+        // Check if options allow the transformation
+
+        // If the function is async and it's not already wrapped, wrap it with try-catch
+        if node.function.is_async {
+            if let Some(body) = &mut node.function.body {
+                if !already_wrapped(body) {
+                    wrap_with_try_catch(body);
                 }
             }
         }
-        current_expr = next_expr;
+
+        // Continue visiting the node
+        node.visit_mut_children_with(self);
     }
-    false
-}
-fn create_catch_arrow_func() -> Expr {
-    Expr::Arrow(ArrowExpr {
-        params: vec![Pat::Ident(BindingIdent {
-            id: Ident {
-                span: DUMMY_SP,
-                sym: "err".into(),
-                optional: false,
-                ctxt: SyntaxContext::empty(),
-            },
-            type_ann: None,
-        })],
-        body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
-            span: DUMMY_SP,
-            stmts: vec![Stmt::Expr(ExprStmt {
-                span: DUMMY_SP,
-                expr: Box::new(Expr::Call(CallExpr {
-                    callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
-                        span: DUMMY_SP,
-                        obj: Box::new(Expr::Ident(Ident {
-                            span: DUMMY_SP,
-                            sym: "console".into(),
-                            optional: false,
-                            ctxt: SyntaxContext::empty(),
-                        })),
-                        prop: MemberProp::Ident(IdentName {
-                            span: DUMMY_SP,
-                            sym: "error".into(),
-                        }),
-                    }))),
-                    args: vec![ExprOrSpread {
-                        spread: None,
-                        expr: Box::new(Expr::Ident(Ident {
-                            span: DUMMY_SP,
-                            sym: "err".into(),
-                            optional: false,
-                            ctxt: SyntaxContext::empty(),
-                        })),
-                    }],
-                    type_args: None,
-                    span: DUMMY_SP,
-                    ctxt: SyntaxContext::empty(),
-                })),
-            })],
-            ctxt: SyntaxContext::empty(),
-        })),
-        is_async: false,
-        is_generator: false,
-        type_params: None,
-        return_type: None,
-        span: DUMMY_SP,
-        ctxt: SyntaxContext::empty(),
-    })
+
+    fn visit_mut_arrow_expr(&mut self, node: &mut ArrowExpr) {
+        wrap_arrow_body_with_try_catch(node);
+        node.visit_mut_children_with(self);
+    }
 }
 
-fn is_then_chain(call_expr: &CallExpr) -> bool {
-    if let Callee::Expr(callee_expr) = &call_expr.callee {
-        if let Expr::Member(MemberExpr { prop, .. }) = &**callee_expr {
-            if let MemberProp::Ident(IdentName { sym, .. }) = prop {
-                return sym == "then";
-            }
-        }
-    }
-    false
-}
 /// An example plugin function with macro support.
 /// `plugin_transform` macro interop pointers into deserialized structs, as well
 /// as returning ptr back to host.
---
+++ tests/fixture/async_test/async_try/output.js
@@ -0,0 +1,8 @@
+async function printFile(filename) {
+    try {
+        let contents = await fs.readFileAsync(filename, 'utf8');
+        console.log(contents);
+    } catch (error) {
+        console.error(this, error);
+    }
+}
